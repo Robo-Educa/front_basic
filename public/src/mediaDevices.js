c57229d69515d22846bff722374e7531bca43e22
Carlos Sales <carlossalesti@gmail.com>
2024-07-08 22:51:21 -0300

    Implementa verificação de permissão de uso de microfone

---
+++ public/src/mediaDevices.js
@@ -0,0 +1,43 @@
+/**
+ * Verifica se existe permissão para uso dos dispositivos de mídia (microfone e/ou câmera)
+ *
+ * @async
+ * @param {string} device - Tipo do dispositivo: 'microphone' OR 'camera'.
+ * @returns {Promise<Object>} - Status atual da permissão: 'granted', 'prompt', 'denied' ou 'notFound'.
+ */
+async function getStatusPermission(device) {
+    let permission;
+    await navigator.permissions
+        .query({ name: device })
+        .then(function (result) { permission = result.state; })
+        .catch(function (error) { permission = "notFound"; });
+    return permission;
+}
+
+/**
+ * Solicita permissão para uso de Microfone
+ *
+ * @async
+  * @returns {string} - 'granted', 'denied', 'notFound' ou 'error'
+ */
+async function micPrompt() {
+    let permission;
+    await navigator.mediaDevices
+        .getUserMedia({
+            audio: true
+        })
+        .then(function (stream) {
+            permission = "granted"        
+        })
+        .catch(function (error) {
+            if (error.message == "Requested device not found") {
+                permission = "notFound";
+            } else if (error.message == "Permission denied") {
+                permission = "denied";
+            } else {
+                console.log(error.message)
+                permission = 'error';
+            }
+        });
+    return permission;
+}
